a8d640b5fa165afefa62e27d09c0481fd6900e65
erick-ochieng <40027564+erick-ochieng@users.noreply.github.com>
2018-07-26 12:08:23 +0300

    Update setup.sh

--- docker-registry/setup.sh
+++ docker-registry/setup.sh
@@ -4,5 +4,4 @@ apt install -y docker
 service docker start
 usermod -aG docker ec2-user
 docker swarm init
-docker service create --replicas 1 --name registry --publish 5000:5000 --publish 8081:8081 sonatype/nexus3:3.6.2
 terraform apply -var-file=variables.tfvars
